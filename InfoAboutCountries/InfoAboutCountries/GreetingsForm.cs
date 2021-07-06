084ea85d92f924e123635ee9340980c04a0802d4
Georgy <Egor-Nikitin02@yandex.ru>
2021-07-06 17:44:09 +0300

    Created GreetingsForm
    Created GreetingsForm to greet a user

---
+++ InfoAboutCountries/InfoAboutCountries/GreetingsForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace InfoAboutCountries
+{
+    public partial class GreetingsForm : Form
+    {
+        public GreetingsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
