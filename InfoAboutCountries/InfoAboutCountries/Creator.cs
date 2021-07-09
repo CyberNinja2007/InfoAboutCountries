00e23f2b3dbd0098266648119808ebdeb81aaf90
Georgy <Egor-Nikitin02@yandex.ru>
2021-07-09 20:21:53 +0300

    Create Creator class
    Created new Creator class and a CreateCountryTable method

---
+++ InfoAboutCountries/InfoAboutCountries/Creator.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+
+namespace InfoAboutCountries
+{
+    public class Creator
+    {
+         public static DataTable CreateCountryTable()
+         { 
+             DataTable resultTable = new DataTable();
+                    
+             resultTable.Columns.Add("Название");
+             resultTable.Columns.Add("Код_страны");
+             resultTable.Columns.Add("Столица");
+             resultTable.Columns.Add("Площадь");
+             resultTable.Columns.Add("Население");
+             resultTable.Columns.Add("Регион");
+        
+             return resultTable;
+         }
+    }
+}
