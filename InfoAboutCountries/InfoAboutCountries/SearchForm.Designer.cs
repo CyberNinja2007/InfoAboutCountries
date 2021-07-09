becf9fb5b1db6981c01195a84f20b60d92e9b649
Georgy <Egor-Nikitin02@yandex.ru>
2021-07-09 15:07:37 +0300

    Created SearchForm
    Created the Search Form to search the coutry

---
+++ InfoAboutCountries/InfoAboutCountries/SearchForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel;
+
+namespace InfoAboutCountries
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.searchText = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Font = new System.Drawing.Font("Arial", 18F);
+            this.searchLabel.Location = new System.Drawing.Point(49, 27);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(300, 27);
+            this.searchLabel.TabIndex = 0;
+            this.searchLabel.Text = "Введите название страны";
+            // 
+            // searchText
+            // 
+            this.searchText.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.searchText.Location = new System.Drawing.Point(49, 83);
+            this.searchText.Name = "searchText";
+            this.searchText.Size = new System.Drawing.Size(300, 20);
+            this.searchText.TabIndex = 1;
+            this.searchText.TextChanged += new System.EventHandler(this.searchText_TextChanged);
+            // 
+            // searchButton
+            // 
+            this.searchButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.searchButton.Enabled = false;
+            this.searchButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.searchButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.searchButton.Location = new System.Drawing.Point(123, 128);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(130, 38);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Поиск";
+            this.searchButton.UseVisualStyleBackColor = false;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(405, 178);
+            this.ControlBox = false;
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchText);
+            this.Controls.Add(this.searchLabel);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(421, 217);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(421, 217);
+            this.Name = "SearchForm";
+            this.Text = "SearchForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox searchText;
+
+        private System.Windows.Forms.Button searchButton;
+
+        private System.Windows.Forms.Label searchLabel;
+
+        #endregion
+    }
+}
---
+++ InfoAboutCountries/InfoAboutCountries/SearchForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace InfoAboutCountries
+{
+    public partial class SearchForm : Form
+    {
+        public SearchForm(bool allInfo)
+        {
+            InitializeComponent();
+        }
+        
+        private void searchText_TextChanged(object sender, EventArgs e)
+        {
+            searchButton.Enabled = Regex.IsMatch(searchText.Text, @"^[A-Z][a-z]+$") ? true : false;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            ResultsForm resultsForm = new ResultsForm( new Loader($"https://restcountries.eu/rest/v2/name/{searchText.Text}"));
+            resultsForm.Show();
+            Close();
+        }
+    }
+}
