cca9fa926f918e99e95bb9d74e532d0c394019b9
Georgy <Egor-Nikitin02@yandex.ru>
2021-07-09 20:35:28 +0300

    Update Loader class
    Updated:
    1. Added main SqlAdapter instead of connectionString
    2. LoadAllCountriesFromDB method
    3. LoadCountryFromURL method
    4. Added new LoadCoutryToDB, CheckCapital, InsertCapital, GetCapital, CheckRegion, InsertRegion, GetRegion, CheckCountry, InsertCountry and UpdateCountry methods

--- InfoAboutCountries/InfoAboutCountries/Loader.cs
+++ InfoAboutCountries/InfoAboutCountries/Loader.cs
@@ -12,18 +12,20 @@ namespace InfoAboutCountries
     public class Loader
     {
         private SqlConnection sqlConnection;
-        private string connectionString;
+        private SqlDataAdapter _adapter;
         private string _url;
 
         public Loader()
         {
-            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
             sqlConnection = new SqlConnection(connectionString); 
         } 
         
         public Loader(string url)
         {
             _url = url;
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString); 
         }
 
         public DataTable LoadData()
@@ -38,7 +40,7 @@ namespace InfoAboutCountries
             }
         }
         
-        public DataTable LoadAllCountriesFromDB()
+        private DataTable LoadAllCountriesFromDB()
         { 
             sqlConnection.Open();
             DataSet ds = new DataSet();
@@ -50,24 +52,25 @@ namespace InfoAboutCountries
 
             try
             {
-                SqlDataAdapter adapter = new SqlDataAdapter(selectAll, sqlConnection);
+                _adapter = new SqlDataAdapter(selectAll, sqlConnection);
                 
-                adapter.Fill(ds);
+                _adapter.Fill(ds);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             
+            sqlConnection.Close();
 
             return ds.Tables[0];
         }
 
-        public DataTable LoadCountryFromURL()
+        private DataTable LoadCountryFromURL()
         {
             string resultJSON;
             List<Country> country;
-            DataTable resultTable = CreateCountryTable();
+            DataTable resultTable = Creator.CreateCountryTable();
 
             using (WebClient wc = new WebClient())
             {
@@ -76,28 +79,211 @@ namespace InfoAboutCountries
                     resultJSON = wc.DownloadString(_url);
                     country = JsonConvert.DeserializeObject<List<Country>>(resultJSON);
                     resultTable.Rows.Add(country[0].name,country[0].alpha2Code,country[0].capital,country[0].area,country[0].population,country[0].region);
+                    return resultTable;
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
                 }
             }
+        }
+        
+        public void LoadCountryToDB(DataTable newData)
+        {
+            int capital;
+            int region;
+
+            sqlConnection.Open();
             
-            return resultTable;
+            capital = !CheckCapital(newData.Rows[0][2].ToString().Trim())
+                ? InsertCapital(newData.Rows[0][2].ToString().Trim()) : GetCapital(newData.Rows[0][2].ToString().Trim());
+            
+            region = !CheckRegion(newData.Rows[0][5].ToString().Trim())
+                ? InsertRegion(newData.Rows[0][5].ToString().Trim()) : GetRegion(newData.Rows[0][5].ToString().Trim());
+
+            if (!CheckCountry(newData.Rows[0][0].ToString().Trim()))
+            {
+                InsertCountry(newData.Rows[0], capital, region);
+            }
+            else
+            {
+                UpdateCountry(newData.Rows[0],capital,region);
+            }
+
+            MessageBox.Show("Успешно добавлено!","",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+            
+            sqlConnection.Close();
         }
 
-        public DataTable CreateCountryTable()
+        private void UpdateCountry(DataRow country, int capital, int region)
         {
-            DataTable resultTable = new DataTable();
+            try
+            {
+                string updateOld = 
+                    "UPDATE Countries.Страны SET Название=@Название,Код_страны=@Код_страны,Столица=@Столица,Площадь=@Площадь,Население=@Население,Регион=@Регион "+
+                    "WHERE Название = @Название";
+                SqlCommand update = new SqlCommand(updateOld,sqlConnection);
             
-            resultTable.Columns.Add("Название");
-            resultTable.Columns.Add("Код_страны");
-            resultTable.Columns.Add("Столица");
-            resultTable.Columns.Add("Площадь");
-            resultTable.Columns.Add("Население");
-            resultTable.Columns.Add("Регион");
+                update.Parameters.AddWithValue("@Название", country[0].ToString().Trim());
+                update.Parameters.AddWithValue("@Код_страны", country[1].ToString().Trim());
+                update.Parameters.AddWithValue("@Столица", capital);
+                update.Parameters.AddWithValue("@Площадь", country[3].ToString().Trim());
+                update.Parameters.AddWithValue("@Население", country[4].ToString().Trim());
+                update.Parameters.AddWithValue("@Регион", region);
+            
+                update.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private bool CheckCountry(string country)
+        {
+            bool haveCountry = false;
+            
+            string findCountry = 
+                $"SELECT * FROM Countries.Страны WHERE Countries.Страны.Название = '{country}'";
+            SqlCommand find = new SqlCommand(findCountry,sqlConnection);
 
-            return resultTable;
+            SqlDataReader sqlDataReader = find.ExecuteReader();
+
+            if (sqlDataReader.Read())
+            {
+                haveCountry = true;
+            }
+            
+            sqlDataReader.Close();
+            
+            return haveCountry;
+        }
+
+        private void InsertCountry(DataRow country, int capital, int region)
+        {
+            try
+            {
+                string insertNew = 
+                    "INSERT INTO Countries.Страны VALUES (@Название,@Код_страны,@Столица,@Площадь,@Население,@Регион)";
+                SqlCommand insert = new SqlCommand(insertNew,sqlConnection);
+            
+                insert.Parameters.AddWithValue("@Название", country[0].ToString());
+                insert.Parameters.AddWithValue("@Код_страны", country[1].ToString());
+                insert.Parameters.AddWithValue("@Столица", capital);
+                insert.Parameters.AddWithValue("@Площадь", country[3].ToString());
+                insert.Parameters.AddWithValue("@Население", country[4].ToString());
+                insert.Parameters.AddWithValue("@Регион", region);
+            
+                insert.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private bool CheckRegion(string region)
+        {
+            bool haveRegion = false;
+            
+            string findRegion = 
+                $"SELECT * FROM Countries.Регионы WHERE Countries.Регионы.Название = '{region}'";
+            SqlCommand find = new SqlCommand(findRegion,sqlConnection);
+
+            SqlDataReader sqlDataReader = find.ExecuteReader();
+
+            if (sqlDataReader.Read())
+            {
+                haveRegion = true;
+            }
+            
+            sqlDataReader.Close();
+            
+            return haveRegion;
+        }
+
+        private int InsertRegion(string region)
+        {
+            
+            string insertcapital = 
+                $"INSERT INTO Countries.Регионы VALUES('{region}')";
+            SqlCommand insert = new SqlCommand(insertcapital,sqlConnection);
+
+            insert.ExecuteNonQuery();
+
+            return GetRegion(region);
+        }
+
+        private int GetRegion(string region)
+        {
+            int regionID = 0;
+            
+            string findRegion = 
+                $"SELECT * FROM Countries.Регионы WHERE Countries.Регионы.Название = '{region}'";
+            SqlCommand find = new SqlCommand(findRegion,sqlConnection);
+
+            SqlDataReader sqlDataReader = find.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                regionID = (int)sqlDataReader.GetValue(0);
+            }
+            
+            sqlDataReader.Close();
+            
+            return regionID;
+        }
+
+        private bool CheckCapital(string capital)
+        {
+            bool haveCapital = false;
+            
+            string findCapital = 
+                $"SELECT * FROM Countries.Города WHERE Countries.Города.Название = '{capital}'";
+            SqlCommand find = new SqlCommand(findCapital,sqlConnection);
+
+            SqlDataReader sqlDataReader = find.ExecuteReader();
+
+            if (sqlDataReader.Read())
+            {
+                haveCapital = true;
+            }
+            
+            sqlDataReader.Close();
+            
+            return haveCapital;
+        }
+
+        private int InsertCapital(string capital)
+        {
+            
+            string insertcapital = 
+                $"INSERT INTO Countries.Города VALUES('{capital}')";
+            SqlCommand insert = new SqlCommand(insertcapital,sqlConnection);
+
+            insert.ExecuteNonQuery();
+
+            return GetCapital(capital);
+        }
+
+        private int GetCapital(string capital)
+        {
+            int capitalID = 0;
+            
+            string findCapital = 
+                $"SELECT * FROM Countries.Города WHERE Countries.Города.Название = '{capital}'";
+            SqlCommand find = new SqlCommand(findCapital,sqlConnection);
+
+            SqlDataReader sqlDataReader = find.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                capitalID = (int)sqlDataReader.GetValue(0);
+            }
+            
+            sqlDataReader.Close();
+            
+            return capitalID;
         }
     }
 }
